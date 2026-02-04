15dd20dfa1982437a8761e0eb7d59b9f7f1882c9
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-02-03 23:05:15 -0500

    chore: update drizzle config for easier production migration

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,11 +1,11 @@
 // https://orm.drizzle.team/docs/get-started/postgresql-new#step-5---setup-drizzle-config-file
 import { defineConfig } from "drizzle-kit";
-import { env } from "./src/env.ts";
 
 // biome-ignore lint/style/noDefaultExport: drizzle config
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
-  dbCredentials: { url: env.DATABASE_URL },
+  // Use process.env for easier migrating in production
+  dbCredentials: { url: process.env["DATABASE_URL"] as string },
 });
